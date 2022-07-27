458160713dd8f1d2f93a57fd834926384f2e93ce
Ranjeet Singh <44586579+jeetsdev@users.noreply.github.com>
2022-07-27 11:38:05 +0530

    no items alert

--- src/pages/ProductListing/ProductListing.jsx
+++ src/pages/ProductListing/ProductListing.jsx
@@ -2,14 +2,15 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Filter, Footer, Header, ProductCard } from "../../components"
 import { useFilter, useProduct } from "../../contexts";
-import { filterProduct, titleHandler } from "../../utils";
+import { ACTION_TYPE, filterProduct, titleHandler } from "../../utils";
 import "./ProductListing.css"
 
 export const ProductListing = () => {
     const { productData } = useProduct();
-    const { filterState } = useFilter();
+    const { filterState, filterDispatch } = useFilter();
     const finalProductData = filterProduct(filterState, productData);
     const { pathname } = useLocation();
+    const { CLEAR_ALL } = ACTION_TYPE;
 
     useEffect(() => {
         // Scrolling to the top of the page
@@ -25,11 +26,17 @@ export const ProductListing = () => {
                 <main className="product__site-wrapper">
                     <Filter />
                     <section className="site__product-sec">
-                        <div className="product__sec-cards grid grid__col-4">
-                            {finalProductData && finalProductData.map(product => {
-                                return <ProductCard key={product.id} product={product} />
-                            })}
-                        </div>
+                        {finalProductData.length === 0 ?
+                            <div className="center__flex product__no-item">
+                                <p>No items found, <span className="btns btn__link" onClick={() => filterDispatch({ type: CLEAR_ALL })}>Clear</span> all filters.</p>
+                            </div>
+                            :
+                            <div className="product__sec-cards grid grid__col-4">
+                                {finalProductData.map(product => {
+                                    return <ProductCard key={product.id} product={product} />
+                                })}
+                            </div>
+                        }
                     </section>
                 </main>
                 <Footer />
