c4ff269548e75cf36a5577f24a01c41a044b727a
Ranjeet Singh <44586579+jeetsdev@users.noreply.github.com>
2022-08-03 19:17:30 +0530

    added error page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,3 +1,14 @@
+import { Link } from "react-router-dom";
+import { ErrorImg } from "../../assets";
+import "./Error.css";
+
 export const Error = () => {
-    return <h1>Still working on that....</h1>
-}
+	return (
+		<div className="center__flex flex__dir-col">
+			<img src={ErrorImg} alt="page not found" className="error-img" />
+			<Link to={"/"} className="btns btn__primary border__rad-4px margin-1rem">
+				Go to Home
+			</Link>
+		</div>
+	);
+};
